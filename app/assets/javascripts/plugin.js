711a266efa496a481dd1d5de4193494be06bf5f0
kevin xu <cming.xu@gmail.com>
2015-08-01 08:30:30 +0800

    MOD

--- app/assets/javascripts/plugin.js
+++ app/assets/javascripts/plugin.js
@@ -9,16 +9,9 @@ building.controller('main', ["$scope", "$http", "$interval", function ($scope, $
   $scope.local_skp_names = "";
 
   $scope.bridge = function (action, param) {
+    console.log('bridge  ' + action);
     window.location.href = "skp:" + action + "@" + param;
   }
-
-  $scope.$watch("skp_names", function (newVal, old) {
-    $scope.bridge('logger', 'newval' + newVal);
-  })
-
-  $scope.$watch("current_model", function (newVal, old) {
-    $scope.bridge('logger', 'newval' + newVal.icon);
-  })
 
   $scope.bridge('current_model', '');
 
